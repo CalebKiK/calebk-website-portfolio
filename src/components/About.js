dad563a1733f2ed2fcdabeca08b1dfef36b8514a
Caleb Karimi <karimicaleb@gmail.com>
2026-01-21 21:48:36 +0300

    Update tech stack and add programming languages icons

--- src/components/About.js
+++ src/components/About.js
@@ -5,24 +5,67 @@ import '../styles/About.css'
 function About() {
 
     const techSkills = [
-        // "HTML",
-        // "CSS",
-        "JavaScript",
-        "Node.js (Express.js)",
-        "React",
-        "Next.js",
-        "CSS",
-        "Github",
-        "Python",
-        "Django",
-        "Flask",
-        "RESTful APIs",
-        "MySQL",
-        "PostgreSQL",
-        "Command Line",
-        // "VS Code",
-        "Docker",
-        "Redis",
+        {
+            name: "JavaScript",
+            icon: "/images/tech-stack-icons/js.png",
+        },
+        {
+            name: "TypeScript",
+            icon: "/images/tech-stack-icons/typescript.png",
+        },
+        {
+            // name: "Node.js (Express.js)",
+            name: "Node.js",
+            icon: "/images/tech-stack-icons/nodejs.png",
+        },
+        {
+            name: "React",
+            icon: "/images/tech-stack-icons/physics.png",
+        },
+        {
+            name: "Next.js",
+            icon: "/images/tech-stack-icons/next-js.jpg",
+        },
+        {
+            name: "Tailwind CSS",
+            icon: "/images/tech-stack-icons/png-transparent-tailwind-css-hd-logo.png",
+        },
+        {
+            name: "GitHub",
+            icon: "/images/tech-stack-icons/github.png",
+        },
+        {
+            name: "Python",
+            icon: "/images/tech-stack-icons/python.png",
+        },
+        {
+            name: "Django",
+            icon: "/images/tech-stack-icons/django-icon.jpg",
+        },
+        {
+            name: "RESTful APIs",
+            icon: "/images/tech-stack-icons/browser.png",
+        },
+        {
+            name: "MySQL",
+            icon: "/images/tech-stack-icons/mysql-database-web-development-computer-software-dolphin.jpg",
+        },
+        {
+            name: "PostgreSQL",
+            icon: "/images/tech-stack-icons/postgresql.jpg",
+        },
+        {
+            name: "Command Line",
+            icon: "/images/tech-stack-icons/command.png",
+        },
+        {
+            name: "Docker",
+            icon: "/images/tech-stack-icons/docker.png",
+        },
+        {
+            name: "Redis",
+            icon: "/images/tech-stack-icons/redis-icon.jpg",
+        },
     ];
 
     const containerVariants = {
@@ -57,7 +100,7 @@ function About() {
                         <img src="/images/Caleb-K Picture.jpg" alt="Caleb K"/>
                     </div>
                     <div className='about-me-description'>
-                        <p>Hello, I’m Caleb Karimi based in Nairobi, Kenya. I am a <span>Web/Software Developer</span> with an <span>Architect’s Mindset</span>. My journey from architecture to software development is more than a career shift - it is a continuation of my passion for creating impactful, well-structured designs. I bring a unique perspective to software development, combining my architectural training with a commitment to building clean, efficient and beautiful digital products.</p>
+                        <p>Hello, I’m Caleb Karimi based in Nairobi, Kenya. I am a <span>Software Engineer</span> with an <span>Architect’s Mindset</span>. My journey from architecture to software development is more than a career shift - it is a continuation of my passion for creating impactful, well-structured designs. I bring a unique perspective to software development, combining my architectural training with a commitment to building clean, efficient and beautiful digital products.</p>
                         <p>I hold a degree in Architecture, a discipline that taught me how to <span>approach complex problems</span>, <span>think critically</span>, and translate <span>abstract ideas into tangible outcomes</span>. These skills naturally extend into my work as a developer, where I apply the same principles of design, collaboration, spatial awareness, communication and problem-solving to create software solutions that are both functional and <span>user-centric</span>.</p>
                         <p>Development is more than just a job for me - <span>it’s a craft</span>. I am committed to continuously learning and evolving with the ever-changing tech landscape, and I thrive on the challenge of <span>pushing the boundaries</span> of <span>what technology can do</span>. Whether it’s building software that drive business growth or developing applications that simplify processes, I pour my enthusiasm and energy into every project.</p>
                     </div>
@@ -72,8 +115,17 @@ function About() {
                         viewport={{ once: false, amount: 0.1 }} 
                     >
                         {techSkills.map((skill, index) => (
-                            <motion.div key={index} className="skill-card" variants={itemVariants}>
-                                <p>{skill}</p>
+                            <motion.div 
+                                key={index} 
+                                className="skill-card" 
+                                variants={itemVariants}
+                            >
+                                <img
+                                    src={skill.icon}
+                                    alt={`${skill.name} icon`}
+                                    className="skill-icon"
+                                />
+                                <p>{skill.name}</p>
                             </motion.div>
                         ))}
                     </motion.div>
